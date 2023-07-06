05845435f0f7562ebafba34bc51121bf352b461c
Elsa <elsahvy@gmail.com>
2023-07-05 17:51:05 -0700

    scroll adjust test

--- src/components/RightSide/Grid/index.jsx
+++ src/components/RightSide/Grid/index.jsx
@@ -2,42 +2,34 @@ import content from "./data";
 
 const Gallery = () => {
   return (
-    <div>
-      <div id="gallery" className="pt-5 pb-[100px]">
-        <section className="flex flex-col pb-[150px]">
-          <div className="mb-5 w-full pl-5 pb-3">
-            <h2 className="md:hidden block font-bold text-3xl text-center text-parchment">
-              Featured Websites
-            </h2>
-          </div>
+    <div id="gallery" className="pt-5 pb-[100px]">
+      <section className="flex flex-col pb-[150px]">
+        <div className="mb-5 w-full pl-5 pb-3">
+          <h2 className="md:hidden block font-bold text-3xl text-center text-parchment">
+            Featured Websites
+          </h2>
+        </div>
 
-          <section className="mt-5 pt-5 md:px-3 px-1 grid 2xl:grid-cols-2 sm:grid-cols-1 gap-5">
-            <>
-              {/* //todo */}
-              <></>
-              {/* //todo */}
-
-              {content.map((item, index) => (
-                <div className="lg:px-5 px-1" key={index}>
-                  <a href={item.url}>
-                    <h2 className="text-2xl md:text-left text-center text-aquamarine drop-shadow-lg p-2 border-0 md:mt-[100px] mt-0">
-                      ({item.id}) {item.title}
-                    </h2>
-                    <img
-                      src={item.image}
-                      alt=""
-                      className="drop-shadow-lg border border-black"
-                    />
-                    <p className="text-parchment drop-shadow-lg p-2">
-                      {item.paragraph1}
-                    </p>
-                  </a>
-                </div>
-              ))}
-            </>
-          </section>
+        <section className="mt-5 pt-5 md:px-3 px-1 grid 2xl:grid-cols-2 sm:grid-cols-1 gap-5">
+          {content.map((item, index) => (
+            <div className="lg:px-5 px-1" key={index}>
+              <a href={item.url}>
+                <h2 className="text-2xl md:text-left text-center text-aquamarine drop-shadow-lg p-2 border-0 md:mt-[100px] mt-0">
+                  ({item.id}) {item.title}
+                </h2>
+                <img
+                  src={item.image}
+                  alt=""
+                  className="drop-shadow-lg border border-black"
+                />
+                <p className="text-parchment drop-shadow-lg p-2">
+                  {item.paragraph1}
+                </p>
+              </a>
+            </div>
+          ))}
         </section>
-      </div>
+      </section>
     </div>
   );
 };
--- src/components/RightSide/index.jsx
+++ src/components/RightSide/index.jsx
@@ -5,7 +5,7 @@ const RightSide = () => {
   return (
     <section
       id="right-side"
-      className="h-screen bg-onyx md:absolute relative right-0 md:w-3/5 w-full overflow-y-scroll p-5"
+      className="h-screen bg-onyx md:absolute relative right-0 md:w-3/5 w-full overflow-y-scroll overflow-x-hidden p-5"
     >
       <div className="md:mt-[150px] mt-2 2xl  :w-3/4 w-full mx-5 px-5">
         <div className="md:block hidden text-parchment">
